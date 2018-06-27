042a319a13188b29db0704f244df83b1922da0c6
Fostire <Rod1389@gmail.com>
2018-06-27 20:38:39 -0300

    Improved outline coloring
    The code that determines the outline's color was simplified.

--- Men.gd
+++ Men.gd
@@ -5,6 +5,14 @@ var blackSide = false; #false for white, true for black
 var king = false;
 var location
 var isSelected = false
+var mustJump = false
+var whiteOutline = Color(0.2, 0.2, 0.9, 1.0)
+var blackOutline = Color(0.6, 0.6, 0.9, 1.0)
+var forcedWhiteOutline = Color(0.2, 0.8, 0.4, 1.0)
+var forcedBlackOutline = Color(0.8, 0.4, 0.6, 1.0)
+var mouseOver = false
+var outlined = false
+var currentColor
 
 
 func _ready():
@@ -15,32 +23,53 @@ func _ready():
 	
 	if side == "black":
 		$Sprite.frame = 0
-		$Sprite.material.set_shader_param("outlineColor",Color(0.6, 0.6, 0.9, 1.0))
-	else:
+		currentColor = blackOutline
+	elif side == "white":
 		$Sprite.frame = 1
-		$Sprite.material.set_shader_param("outlineColor",Color(0.2, 0.2, 0.9, 1.0))
+		currentColor = whiteOutline
+		
+	$Sprite.material.set_shader_param("outlineColor", currentColor)
 
 func _process(delta):
-	if isSelected:
-		$Sprite.material.set_shader_param("outlineSize",0.013)
-		if side == "white":
+	if mustJump:
+		outlined = true
+		if side == "black":
+			currentColor = forcedBlackOutline
+		elif side == "white":
+			currentColor = forcedWhiteOutline
+	
+	if mouseOver or isSelected:
+		if side == "black":
+			currentColor = blackOutline
+		elif side == "white":
+			currentColor = whiteOutline
+	
+	if outlined:
+		if side == "black":
+			$Sprite.material.set_shader_param("outlineSize",0.013)
+		elif side == "white":
 			$Sprite.material.set_shader_param("outlineSize",0.015)
+	elif not outlined:
+		$Sprite.material.set_shader_param("outlineSize", 0)
+	$Sprite.material.set_shader_param("outlineColor", currentColor)
 
 
 func _on_Men_mouse_entered():
-	if not isSelected:
-		$Sprite.material.set_shader_param("outlineSize",0.013)
-		if side == "white":
-			$Sprite.material.set_shader_param("outlineSize",0.015)
-
+	mouseOver = true
+	outlined = true
 
 func _on_Men_mouse_exited():
-	if not isSelected:
-		$Sprite.material.set_shader_param("outlineSize",0)
+	mouseOver = false
+	if not isSelected or not mustJump:
+		outlined = false
+
+func select():
+	isSelected = true
+	outlined = true
 
 func deselect():
 	isSelected = false
-	$Sprite.material.set_shader_param("outlineSize",0)
+	outlined = false
 	
 func king():
 	king = true
